0ef265874608454d2e38207adcd1512535a0fe11
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2020-02-04 22:48:46 +0100

    array + loop solutions in vue and js

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -9,3 +9,27 @@ const vm = new Vue({
     theSnowBurger: "The Snow Burger"
  } 
 })
+
+let menu = [theVeganBurger, theAffesSpecial, theChickenBurger, theSchneeBurger, theSnowBurger];
+
+const vm = new Vue({
+el: '#myID',
+data: {
+	menu
+	}
+})  
+
+<div id="myID">
+    <h1>Välj en burgare</h1>
+    
+    <li v-for="burger in menu">
+      {{burger.name}}
+       <p v-if="burger.gluten">
+       Contains Gluten
+       </p>
+      <p v-if="burger.lactose">
+      Contains Lactose
+      </p>
+    </li>
+</div>
+
