a099c7a40c503bc68a207c1cedb9150b5163b517
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2020-02-15 11:49:47 +0100

    Added the map

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,50 +1,18 @@
-/*
-const vm = new Vue({
- el: '#myID',
- data: {
- 		theVeganBurger: "The Vegan Burger",
- 		theAffesSpecial: "The Affes Special",
-    theChickenBurger: "The Chicken Burger",
-    theSchneeBurger: "The Schnee Burger",
-    theSnowBurger: "The Snow Burger"
- } 
-})
-
-let menu = [theVeganBurger, theAffesSpecial, theChickenBurger, theSchneeBurger, theSnowBurger];
-
-const vm = new Vue({
-el: '#myID',
-data: {
-	menu
-	}
-})  
-
-<div id="myID">
-    <h1>Välj en burgare</h1>
-    
-    <li v-for="burger in menu">
-      {{burger.name}}
-       <p v-if="burger.gluten">
-       Contains Gluten
-       </p>
-      <p v-if="burger.lactose">
-      Contains Lactose
-      </p>
-    </li>
-</div>
-*/
-
 /*const vm = new Vue({
-    el: '#wrapper',
-    data: {
-        burger1: theVeganBurger,
-        burger2: theAffesSpecial,
-        burger3: theChickenBurger,
-        burger4: theSchneeBurger,
-        burger5: theSnowBurger,
-        vueMenu: menu
-    }
-    })*/
+  el: '#wrapper',
+  data: {
+  burger1: theVeganBurger,
+  burger2: theAffesSpecial,
+  burger3: theChickenBurger,
+  burger4: theSchneeBurger,
+  burger5: theSnowBurger,
+  vueMenu: menu
+  }
+  })*/
+
+
+'use strict';
+const socket = io();
 
 //Here we use json list
 const vm = new Vue({
@@ -53,18 +21,62 @@ const vm = new Vue({
         food: food,
         name: '',
         email: '',
-        streetname: '',
-        housenumber: '',
+        // streetname: '',
+        // housenumber: '',
         paymentOption: '',
         theGender: '',
         showOrder: false,
-        checked: []
+        checked: [],
+        orders: {}
     },
-    
+    created: function() {
+        /* When the page is loaded, get the current orders stored on the server.
+         * (the server's code is in app.js) */
+        socket.on('initialize', function(data) {
+            this.orders = data.orders;
+        }.bind(this));
+
+        /* Whenever an addOrder is emitted by a client (every open map.html is
+         * a client), the server responds with a currentQueue message (this is
+         * defined in app.js). The message's data payload is the entire updated
+         * order object. Here we define what the client should do with it.
+         * Spoiler: We replace the current local order object with the new one. */
+        socket.on('currentQueue', function(data) {
+            this.orders = data.orders;
+        }.bind(this));
+    },
     methods: {
+        getNext: function() {
+            /* This function returns the next available key (order number) in
+             * the orders object, it works under the assumptions that all keys
+             * are integers. */
+            let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+                return Math.max(last, next);
+            }, 0);
+            return lastOrder + 1;
+        },
+        addOrder: function(event) {
+            /* When you click in the map, a click event object is sent as parameter
+             * to the function designated in v-on:click (i.e. this one).
+             * The click event object contains among other things different
+             * coordinates that we need when calculating where in the map the click
+             * actually happened. */
+            let offset = {
+                x: event.currentTarget.getBoundingClientRect().left,
+                y: event.currentTarget.getBoundingClientRect().top,
+            };
+            socket.emit('addOrder', {
+                orderId: this.getNext(),
+                details: {
+                    x: event.clientX - 10 - offset.x,
+                    y: event.clientY - 10 - offset.y,
+                },
+                orderItems: ['Beans', 'Curry'],
+            });
+        },
         markDone: function() {
             orderInformation();
             this.showOrder = true;
         }
     }
-})
+});
